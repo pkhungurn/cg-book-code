800effe3bd0e9419a6d7ba9ef0203adc5ae967a4
dragonmeteor <dragonmeteor@gmail.com>
2023-12-10 19:26:45 +0900

    more work

--- chapter-08/program-03/src/index.js
+++ chapter-08/program-03/src/index.js
@@ -4,6 +4,6 @@ var gl = canvas[0].getContext("webgl2");
 if (!gl) {
     alert("Cannot get WebGL 2 context!");
 } else {
-    gl.clearColor(1.0, 0.0, 0.0, 0.0);
+    gl.clearColor(1.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
