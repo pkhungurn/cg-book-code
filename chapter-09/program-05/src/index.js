f41728a7e7a2f0ea2d6a83cd24420eacd94802ce
dragonmeteor <dragonmeteor@gmail.com>
2024-03-24 22:57:49 +0900

    more work

--- chapter-09/program-05/src/index.js
+++ chapter-09/program-05/src/index.js
@@ -14,15 +14,16 @@ async function createProgram(gl) {
 
 function createVertexBuffer(gl) {
     // Step 1: Create an array containing the data.
-    var vertexData = [
+    let vertexData = [
         -0.5, -0.5, 0.0,
         0.5, -0.5, 0.0,
-        0.5, 0.5, 0.0
+        0.5, 0.5, 0.0,
+        -0.5, 0.5, 0.0
     ];
     // Step 2: Create a Float32Array from the data.
-    var vertexArray = new Float32Array(vertexData);
+    let vertexArray = new Float32Array(vertexData);
     // Step 3: Create a buffer object.
-    var vertexBuffer = gl.createBuffer();
+    let vertexBuffer = gl.createBuffer();
     // Step 4: Bind the buffer to the ARRAY_BUFFER target.
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     // Step 5: Transfer the buffer data.
@@ -33,7 +34,26 @@ function createVertexBuffer(gl) {
     return vertexBuffer;
 }
 
-function updateWebGL(gl, program, vertexBuffer) {
+function createIndexBuffer(gl) {
+    // Step 1: Create an array containing the data.
+    let indexData = [
+        0, 2, 3, 0, 1, 2,
+    ]
+    // Step 2: Create a Uint32Array (or Uint16Array) from the data.
+    let indexArray = new Uint32Array(indexData);
+    // Step 3: Create a buffer object.
+    let indexBuffer = gl.createBuffer();
+    // Step 4: Bind the buffer to the ELEMENT_ARRAY_BUFFER target.
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    // Step 5: Transfer the buffer data.
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexArray, gl.STATIC_DRAW);
+    // Step 6: Unbind the buffer.
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    return indexBuffer;
+}
+
+function updateWebGL(gl, program, vertexBuffer, indexBuffer) {
     gl.clearColor(0.0, 0.0, 0.0, 0.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -52,12 +72,16 @@ function updateWebGL(gl, program, vertexBuffer) {
     gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 3 * 4, 0);
     // Step 6: Unbind the buffer.
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    // Step 7: Draw.
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-    // Step 8: Unuse the program.
+    // Step 7: Bind the index buffer.
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    // Step 8: Draw with drawElements.
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0);
+    // Step 9: Unbind the indexbuffer.
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    // Step 10: Unuse the program.
     gl.useProgram(null);
 
-    window.requestAnimationFrame(() => updateWebGL(gl, program, vertexBuffer));
+    window.requestAnimationFrame(() => updateWebGL(gl, program, vertexBuffer, indexBuffer));
 }
 
 let canvas = $("#webglCanvas");
@@ -66,5 +90,6 @@ if (!gl) {
     alert("Cannot get WebGL 2 context!");
 } else {
     let vertexBuffer = createVertexBuffer(gl);    
-    createProgram(gl).then(program => updateWebGL(gl, program, vertexBuffer));
+    let indexBuffer = createIndexBuffer(gl);
+    createProgram(gl).then(program => updateWebGL(gl, program, vertexBuffer, indexBuffer));
 }
